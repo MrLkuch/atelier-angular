83f80ebf1a420df6e4571000736548ab6f6d6ef8
MrLkuch <kucheida.lucas@gmail.com>
2024-06-17 14:59:18 +0200

    feat(app.component): change app html & class to use the new service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BtnIncrementComponent } from './btn-increment/btn-increment.component'
+import { BtnIncrementComponent } from './btn-increment/btn-increment.component';
+import { BtnDecrementComponent } from './btn-decrement/btn-decrement.component';
+import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    BtnIncrementComponent],
+    BtnIncrementComponent,
+    BtnDecrementComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(public counterService: CounterService) { }
   title = 'atelier-angular';
 }
