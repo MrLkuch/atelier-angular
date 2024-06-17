6a2c830aadb9e902ba22832277cacf7bde0e1240
MrLkuch <kucheida.lucas@gmail.com>
2024-06-17 12:34:21 +0200

    feat(): add increment btn to main app html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BtnIncrementComponent } from './btn-increment/btn-increment.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    BtnIncrementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
